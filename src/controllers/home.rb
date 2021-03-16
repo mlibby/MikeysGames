4b128d00c28615cd235f65f02d94c3d9de23c301
Michael C. Libby <m@mlibby.com>
2021-03-15 20:44:44 -0500

    trying to get a feel for how to run a server and play views

--- src/controllers/home.rb
+++ src/controllers/home.rb
@@ -1,8 +1,27 @@
+require "socket"
+
+CurrentGame = nil
+
 module MikeysGames
   class App < Sinatra::Base
+    addr = Socket.ip_address_list.detect { |addr| addr.ipv4_private? }
+    server = "#{addr.ip_address}:#{settings.port}"
+
     get "/" do
-      begin
-        erb_layout :"home/index"
+      if CurrentGame.nil?
+        erb_layout :"home/play"
+      else
+        erb_layout :"pathwords/play"
+      end
+    end
+
+    get "/serve" do
+      addr = Socket.ip_address_list.detect { |addr| addr.ipv4_private? }
+      @server = "#{addr.ip_address}:#{settings.port}"
+      if CurrentGame.nil?
+        erb_layout :"home/serve"
+      else
+        erb_layout :"pathwords/serve"
       end
     end
   end

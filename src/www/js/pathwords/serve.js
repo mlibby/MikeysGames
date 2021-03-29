6f261c8f92817e899cb6a31d3278f7b04562adce
Michael C. Libby <m@mlibby.com>
2021-03-28 19:16:10 -0500

    Pathwords: adding stage classes to .../serve page

--- src/www/js/pathwords/serve.js
+++ src/www/js/pathwords/serve.js
@@ -3,6 +3,12 @@ let playersExpected;
 let playersNeeded;
 let playersJoining;
 let playerList;
+let stageZero;
+let stageOne;
+let stageTwo;
+let stageThree;
+let startsIn;
+let timeLeft;
 
 document.addEventListener("DOMContentLoaded", function (event) {
   playerCount = document.querySelector("#player-count");
@@ -10,9 +16,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
   playersNeeded = document.querySelector("#players-needed");
   playersJoining = document.querySelector("#players-joining");
   playerList = document.querySelector("#player-list");
+  stageZero = document.querySelectorAll(".stage-0");
+  stageOne = document.querySelectorAll(".stage-1");
+  stageTwo = document.querySelectorAll(".stage-2");
+  stageThree = document.querySelectorAll(".stage-3");
+  startsIn = document.querySelector("#starts-in");
+  timeLeft = document.querySelector("#time-left");
 
   getUpdate();
 });
+
+function show(nodes) {
+  nodes.forEach((node) => {
+    node.style.display = "block";
+  })
+}
+
+function hide(nodes) {
+  nodes.forEach((node) => {
+    node.style.display = "none";
+  })
+}
 
 function getUpdate() {
   setTimeout(() => {
@@ -23,16 +47,44 @@ function getUpdate() {
 }
 
 function displayUpdate(response) {
-  if (response.playersExpected > 0) {
-    playersNeeded.style.display = "none";
-    playersJoining.style.display = "block";
+  if (response.stage == 0) {
+    show(stageZero);
+    hide(stageOne);
+    hide(stageTwo);
+    hide(stageThree);
 
-    playerCount.innerText = response.playerCount;
-    playersExpected.innerText = response.playersExpected;
+    if (response.playersExpected > 0) {
+      playersNeeded.style.display = "none";
+      playersJoining.style.display = "block";
+
+      playerCount.innerText = response.playerCount;
+      playersExpected.innerText = response.playersExpected;
+    }
+    else {
+      playersNeeded.style.display = "block";
+      playersJoining.style.display = "none";
+    }
   }
-  else {
-    playersNeeded.style.display = "block";
-    playersJoining.style.display = "none";
+  else if (response.stage == 1) {
+    hide(stageZero);
+    show(stageOne);
+    hide(stageTwo);
+    hide(stageThree);
+    startsIn.innerText = response.timer;
+  }
+  else if (response.stage == 2) {
+    hide(stageZero);
+    hide(stageOne);
+    show(stageTwo);
+    hide(stageThree);
+    timeLeft.innerText = response.timer;
+  }
+  else if (response.stage == 3) {
+    hide(stageZero);
+    hide(stageOne);
+    hide(stageTwo);
+    show(stageThree);
+    timeLeft.innerText = response.timer;
   }
 
   playerList.replaceChildren();
